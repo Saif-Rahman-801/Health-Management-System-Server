afdc65f92be682fc48811a59ffe7106197685847
Saif-Rahman-801 <saifurs102@gmail.com>
2024-04-02 15:57:03 +0600

    login function written, registration bug fixed

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,9 +2,29 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {
-  uploadOnCloudinary,
-} from "../utils/cloudinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    // get the user to generate token for each user
+    const user = await User.findById(userId);
+
+    // generate token using the methods you created in the user.model.js
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // save the user with refresh token in the db
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false }); // it disables the validation checks defined in your schema before saving the document to the database.
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating access or refresh token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // check if the user exists
@@ -15,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const existedUser = await User.findOne({
-    $or: [{ username }, { email }],
+    email
   });
 
   if (existedUser) throw new ApiError(400, "User already exists");
@@ -29,7 +49,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   // upload on cloudinary
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-
 
   //   Create user
   const user = await User.create({
@@ -50,4 +69,51 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registration successful"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  // check if the user exists or not
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(404, "user doesn't exists");
+  }
+
+  // check if the password matches
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Password Doesn't match");
+  }
+
+  // generate and get the jwt tokens to set them in the cookie
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user?._id
+  );
+
+  // get the logged in user
+  const loggedInUser = await User.findById(user?._id).select(
+    "-password -refreshToken"
+  );
+
+  // send the response with setting the jwt in the cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User loggedIn successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
