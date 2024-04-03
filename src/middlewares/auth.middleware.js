24add993b45aee8e42a8c7085a62f9970e9eb3a4
Saif-Rahman-801 <saifurs102@gmail.com>
2024-04-03 17:11:44 +0600

    auth middleware written

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,43 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJwt = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log(decodedToken);
+    /* 
+    the decoded token looks like 
+    decodedToken = {
+    "_id": "60f23c9d7a3b1935a875d71f", // Example user ID
+    "email": "example@example.com", // Example email
+    "username": "example_username", // Example username
+    "iat": 1649159267, // Issued at (timestamp)
+    "exp": 1649162867 // Expiry (timestamp)
+    }
+     */
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      // frontend discuss
+      throw new ApiError(401, "Invalid user token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
