79a1892feb8f052db06f6f4a3882db4111109d5d
Saif-Rahman-801 <saifurs102@gmail.com>
2024-04-17 14:20:46 +0600

    users fetched for the admin

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -14,4 +16,18 @@ const isAdminTrue = asyncHandler(async (req, res) => {
   );
 });
 
-export { isAdminTrue };
+const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find({});
+    console.log(users);
+    res
+      .status(200)
+      .json(
+        new ApiResponse(200, { users }, "users fetched successfully")
+      );
+  } catch (error) {
+    throw new ApiError(500, `server error while fetching users; ${error.message}`);
+  }
+});
+
+export { isAdminTrue, getAllUsers };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { isAdminTrue } from "../controllers/admin.controllers.js";
+import { getAllUsers, isAdminTrue } from "../controllers/admin.controllers.js";
 import { isUserAvailable } from "../middlewares/isUserAvailable.middleware.js";
 import { isAdmin } from "../middlewares/isAdmin.middleware.js";
 
 const router = Router();
 
 router.route("/admin-info").get(verifyJwt, isUserAvailable, isAdmin, isAdminTrue);
+
+router.route("/users").get(verifyJwt, isUserAvailable, isAdmin, getAllUsers);
 
 export default router;
